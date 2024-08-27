c8085bd25c65ace768970e6170c0bba9c0d73bed
Puneet-kaushikji <puneetkaushik177@gmail.com>
2024-08-27 17:32:04 +0530

    conti5

--- server/controllers/auth-controller.js
+++ server/controllers/auth-controller.js
@@ -14,7 +14,10 @@ const home = async (req,res) =>{
 // ####### Registration Logic  #######
  const register = async (req,res) =>{
     try{
-        res.status(200).send("welcome to registration page using contoller");
+        console.log(req.body);
+        res
+        .status(200)
+        .json({message:req.body});
     }
     catch (error){
         res.status(400).send({msg:"page not found"})
