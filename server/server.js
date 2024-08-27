29c95b98a66552a3b6b13396edacf2ec741b215f
Puneet-kaushikji <puneetkaushik177@gmail.com>
2024-08-27 18:10:08 +0530

    conti6

--- server/server.js
+++ server/server.js
@@ -2,15 +2,11 @@ const express = require("express");
 const app = express();
 const router = require("./router/auth-router");
 
+
+app.use(express.json());
+
+
 app.use("/api/auth", router);
-
-app.get("/", (req, res) => {
-    res.status(200).send("welcome to my world here and there");
-});
-
-app.get("/register", (req, res) => {
-    res.status(200).send("welcome to registration page");
-});
 
 const PORT = 5000;
 
