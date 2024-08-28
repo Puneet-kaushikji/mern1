135767017516c22ad372be9ce4ce3f154a4be2fa
Puneet-kaushikji <puneetkaushik177@gmail.com>
2024-08-28 11:42:56 +0530

    conti6

---
+++ server/utils/db.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const URI ="mongodb://127.0.0.1:27017/mern_admin";
+// mongoose.connect(URI);
+
+const connectDb = aync () => {
+    try {
+
+    } catch (error) {
+        
+    }
+}
