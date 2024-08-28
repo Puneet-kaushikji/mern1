573e0de2b8fa1c34930524bbec7b68997b9aae35
Puneet-kaushikji <puneetkaushik177@gmail.com>
2024-08-28 17:27:18 +0530

    conti7

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const router = require("./router/auth-router");
-
+const connectDB = require("./utils/db");
 
 app.use(express.json());
 
@@ -10,8 +10,12 @@ app.use("/api/auth", router);
 
 const PORT = 5000;
 
+
+connectDB().then(()=>{ 
+
 app.listen(PORT, () => {
     console.log(`Server is running at port: ${PORT}`);
 });
+}); 
 
 
--- server/utils/db.js
+++ server/utils/db.js
@@ -3,10 +3,14 @@ const mongoose = require("mongoose");
 const URI ="mongodb://127.0.0.1:27017/mern_admin";
 // mongoose.connect(URI);
 
-const connectDb = aync () => {
+const connectDb = async () => {
     try {
-
+        await mongoose.connect(URI);
+        console.log("connection successful to DB")
     } catch (error) {
-        
+        console.error("database connection failed");
+        process.exit(0);
     }
-}
+};
+
+module.exports = connectDB;
