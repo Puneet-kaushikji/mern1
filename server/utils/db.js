3444d735ea331b3a3a6847c93247b7d1db9f828d
Puneet-kaushikji <puneetkaushik177@gmail.com>
2024-08-29 18:32:05 +0530

    conti8

--- server/utils/db.js
+++ server/utils/db.js
@@ -1,14 +1,18 @@
 const mongoose = require("mongoose");
 
-const URI ="mongodb://127.0.0.1:27017/mern_admin";
+// const URI ="mongodb://127.0.0.1:27017/mern_admin";
 // mongoose.connect(URI);
+
+const URI = process.env.MONGODB_URI;
+
+
 
 const connectDb = async () => {
     try {
         await mongoose.connect(URI);
-        console.log("connection successful to DB")
+        console.log("Connection successful to DB");
     } catch (error) {
-        console.error("database connection failed");
+        console.error("Database connection failed", error);
         process.exit(0);
     }
 };
