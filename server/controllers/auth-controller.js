0b27e707773181917318ec41003132be688d2971
Puneet-kaushikji <puneetkaushik177@gmail.com>
2024-09-03 15:28:05 +0530

    conti12

--- server/controllers/auth-controller.js
+++ server/controllers/auth-controller.js
@@ -1,3 +1,6 @@
+const User = require("../models/user-models");
+
+
 
 //###########    home route logic ######3
 const home = async (req,res) =>{
@@ -15,12 +18,23 @@ const home = async (req,res) =>{
  const register = async (req,res) =>{
     try{
         console.log(req.body);
-        res
-        .status(200)
-        .json({message:req.body});
+        const {username,email,phone,password } = req.body;
+
+
+        const userExist = await User.findOne({email:email});
+
+
+        if(userExist){
+            return res.status(400).json({msg:"email already exists"});
+        }
+
+
+       const userCreated = await User.create({username,email,phone,password});
+
+        res.status(200).json({msg : userCreated});
     }
     catch (error){
-        res.status(400).send({msg:"page not found"})
+        res.status(500).send({msg:"internal server error"})
     }
  }
 
