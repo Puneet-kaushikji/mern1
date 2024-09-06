5624c61866fc16ae350813438b8cd4c2c23ebbd9
Puneet-kaushikji <puneetkaushik177@gmail.com>
2024-09-06 15:06:17 +0530

    login1

--- server/router/auth-router.js
+++ server/router/auth-router.js
@@ -10,6 +10,9 @@ router.route("/").get(authcontrollers.home);
 
 
 router.route("/register").post(authcontrollers. register);
+router.route("/login").post(authcontrollers. login);
+
+
 
 // app.get("/register",(req,res)=>{
 //     res.status(200).send("welcome to registration page");
