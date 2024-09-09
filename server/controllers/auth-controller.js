bb794f2b42ad0fe65e063b86df91eaa4719f822a
Puneet-kaushikji <puneetkaushik177@gmail.com>
2024-09-09 18:35:09 +0530

    mm

--- server/controllers/auth-controller.js
+++ server/controllers/auth-controller.js
@@ -14,7 +14,7 @@ const home = async (req,res) =>{
     }
 }
 
-// ####### Registration Logic  #######
+// ####### user Registration Logic  #######
  const register = async (req,res) =>{
     try{
         console.log(req.body);
@@ -43,5 +43,36 @@ const home = async (req,res) =>{
         res.status(500).send({msg:"internal server error"});
     }
  }
+
+
+ //####### user Login Logic  #######
+ const login = async (req,res) => {
+    try{
+        const {email,password} = req.body;
+
+        const userExist = await User.findOne({email});
+
+        if(!userExist){
+            return res.status(400).json({message:"Invalid credentials"});
+        }
+
+        //const user = await bcrypt.compare(password,userExist.password);
+        const isPasswordValid = await userExist.comparePassword(password);
+
+        if(isPasswordValid){
+            register.status(200).json({
+                message:"Login Successful",
+                token: await userExist.generateToken(),
+                userId:userExist._idtoString(),
+            });
+        }
+        else {
+            res.status(401).json({message:"Invalid email or password"});
+        }
+
+    } catch(error){
+        res.status(500).json({message:"Internal serve error"});
+    }
+ };
 
 module.exports = {home , register};
