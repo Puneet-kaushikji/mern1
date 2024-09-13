ade998c965fd5539871151fd01470ebb1a64ebff
Puneet-kaushikji <puneetkaushik177@gmail.com>
2024-09-13 12:18:03 +0530

    middleware2

---
+++ server/middlewares/validate-middleware.js
@@ -0,0 +1,28 @@
+const validate = (schema) => async (req,res,next) => {
+    try{
+        const parseBody = await schema.parseAsync(req.body);
+        req.body = parseBody;
+        return next();
+
+    }catch(err){
+        const status = 422;
+        const message = "fill the input properly";
+        const extraDetails = err.issues.map((curElem) => curElem.message);
+
+
+        const error = {
+            status, 
+            message,
+            extraDetails,
+        };
+
+        if(error){
+            return res.json(error.extraDetails)
+        }
+
+
+        next(extraDetails);
+    }
+};
+
+module.exports = validate;
--- server/router/auth-router.js
+++ server/router/auth-router.js
@@ -2,14 +2,17 @@
 const express = require("express");
 const router = express.Router();
 const authcontrollers = require("../controllers/auth-controller");
+const validate = require("../middlewares/validate-middleware");
+const signupSchema = require("../validators/auth-validator");
 
 
 
 
 router.route("/").get(authcontrollers.home);
 
-
-router.route("/register").post(authcontrollers.register);
+router
+    .route("/register")
+    .post(validate(signupSchema), authcontrollers.register);
 router.route("/login").post(authcontrollers.login);
 
 
