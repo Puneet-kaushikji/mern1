7fd892fd4eae6e246d64e6bd191932a677e3bbb8
Puneet-kaushikji <puneetkaushik177@gmail.com>
2024-09-24 18:28:14 +0530

    17video

---
+++ server/models/contact-model.js
@@ -0,0 +1,7 @@
+const {Schema , model} = require('mongoose');
+const contactSchema = new Schema({username:{ type: String,required:true}, email:{
+    message:{type: String, required:true},}
+});
+
+const Contact = new model("Contact",contactSchema);
+module.exports = Contact;
--- server/server.js
+++ server/server.js
@@ -5,10 +5,12 @@ const router = require("./router/auth-router");
 const connectDB = require("./utils/db");
 const errorMiddleware = require("./middlewares/error-middleware");
 
+// to get the json data in express app.
 app.use(express.json());
 
-
+// Mount the Router: To use the router in your main Express app, you can "mount" it at a specific URL prefix
 app.use("/api/auth", router);
+app.use("/api/form", contactRoute);
 
 app.use(errorMiddleware);
 
