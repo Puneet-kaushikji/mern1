131b6b4ba1d390ed65226c5e057822120823b46b
puneet-kaushikji <puneetkaushik177@gmail.com>
2024-12-22 00:03:22 +0530

    continue

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -70,7 +70,7 @@ export const Login = () => {
                   </div>
                   <br />
                   <button type="submit" className="btn btn-submit">
-                    Register Now
+                   Login Now
                   </button>
                 </form>
               </div>
